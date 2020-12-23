0dbf9b93c74f4456365769556ee71aaab4883818
Lovell Felix <lovell2juicy@yahoo.com>
2020-12-22 23:03:14 -0600

    update api version to S3v4

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-mc alias set deploy $MINIO_ENDPOINT $MINIO_ACCESS_KEY $MINIO_SECRET_KEY
+mc alias set deploy $MINIO_ENDPOINT $MINIO_ACCESS_KEY $MINIO_SECRET_KEY --api S3v4
 
 mc mirror --overwrite $1 "deploy/$2"
